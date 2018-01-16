bcf25b59f7ed1144e830561a9375b7354977183b
taka <taka@taka.co.jp>
2018-01-16 13:37:12 +0900

    first commit

---
+++ EXEC_SSRS.bat
@@ -0,0 +1,14 @@
+@echo off 
+::Script Variables 
+SET LOGFILE=%~dp0_Log_%now%.txt
+SET REPORTSERVER=http://ec2amaz-ufdc2a1/ReportServer_SKC
+SET RS="RS.EXE" 
+SET TIMEOUT=60
+
+::Clear Log file
+IF EXIST %logfile% DEL %logfile%
+::Run Scripts
+ %rs% -i %~dp0SSRSDeploy.rss -s "%REPORTSERVER%"
+ECHO Finished Load at TE% %TIME% >>%LOGFILE%
+ECHO. >>%LOGFILE%
+PAUSE
